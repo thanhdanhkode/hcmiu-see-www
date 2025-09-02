0eecc65f8d9af1584939eca740723460ca0a09f9
thanhdanhkode <bfffamilk@gmail.com>
2025-09-02 20:22:13 +0700

    chore: update ui for landing page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,32 +1,32 @@
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Header } from '@/features/Landing/Header'
 import { Section } from '@/features/Landing/Section'
-import { FAQ } from '@/templates/FAQ'
+import { Footer } from '@/templates/Footer'
+import Image from 'next/image'
 
 export default function () {
   return (
     <>
       <Header />
-      {/* <Section>
-        <FAQ
-          FAQ={[
-            {
-              id: 'question-1',
-              question: 'What is your return policy?',
-              answer: 'Our return policy is 30 days no questions asked.',
-            },
-            {
-              id: 'question-2',
-              question: 'What is your shipping policy?',
-              answer: ['We offer free shipping on all orders over $50.', 'Standard shipping takes 5-7 business days.'],
-            },
-            {
-              id: 'question-3',
-              question: 'What is your return policy?',
-              answer: 'Our return policy is 30 days no questions asked.',
-            },
-          ]}
-        />
-      </Section> */}
+      <div className="px-3 xl:px-6 mt-3 w-full relative">
+        <AspectRatio
+          ratio={24 / 10}
+          className="relative w-full [&_img]:rounded-md"
+        >
+          <Image
+            src={
+              'https://w.ladicdn.com/s1440x717/5ed55ee180bb8b3b45fbd94c/202508-cover_iuwf25_2048x898_2-20250823160109-4ou7h.png'
+            }
+            alt=""
+            fill
+            objectFit="cover"
+          />
+        </AspectRatio>
+      </div>
+      <Section>
+        <div></div>
+      </Section>
+      <Footer />
     </>
   )
 }
